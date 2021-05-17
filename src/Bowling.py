4d8f345138a3cc1925b93603caca27a0cbdb514a
Martin Carlsson <martin@funnel.io>
2021-05-17 18:23:54 +0200

    All green.

--- src/Bowling.py
+++ src/Bowling.py
@@ -1,13 +1,22 @@
 def score(rolls: str):
-    prev_current_next_nextagain = list(zip(" " + rolls[:-1], rolls, rolls[1:]+" ", rolls[2:]+"  "))
+    prev_current_next_nextagain = list(zip("0" + rolls[:-1], rolls[0:10], rolls[1:]+"0", rolls[2:]+"00"))
 
     score = 0
     for prev_roll, current_roll, next_roll, next_again_roll in prev_current_next_nextagain:
         if current_roll == 'X':
-            score += 10 + int(next_roll) + int(next_again_roll)
+            score += _score(current_roll) + _score(next_roll) + _score(next_again_roll)
         elif current_roll == '/':
-            score += 10 - int(prev_roll) + int(next_roll)
+            score += 10 - _score(prev_roll) + _score(next_roll)
         else:
-            score += int(current_roll)
+            score += _score(current_roll)
 
     return score
+
+def _score(roll: str):
+    if roll == 'X':
+        return 10
+    else:
+        return int(roll)
+
+
+
